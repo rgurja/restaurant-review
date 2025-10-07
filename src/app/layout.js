5055b0b62c36ff6efe55a82b936c7fd04128fd17
rgurja <rgurja@bearcubs.santarosa.edu>
2025-10-06 22:15:41 -0700

    add authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "@/src/app/styles.css";
 import Header from "@/src/components/Header.jsx";
 import { getAuthenticatedAppForUser } from "@/src/lib/firebase/serverApp";
+
 // Force next.js to treat this route as server-side rendered
 // Without this line, during the build process, next.js will treat this route as static and build a static HTML file for it
 export const dynamic = "force-dynamic";
